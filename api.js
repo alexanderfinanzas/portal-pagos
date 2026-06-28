ff046c2143ebcf656a111fdd6df19ae1e3bf0ac0
alexanderfinanzas <finanzas@grupotsalach.com>
2026-06-28 16:01:01 -0600

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const Api = {
   // Reemplazar por la URL real de tu Google Apps Script
-  API_URL: 'https://script.google.com/macros/s/TU_SCRIPT_ID/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbz1V6S3R_zS5DFm6LkDRHN66U23ABw43dM0jc44_Fq6Z7-4TsTd3TZoOLzSOUveL-7p/exec',
 
   fetchData: async () => {
     try {
